baf831f63c2c4a3ceb0de4abb11fc20adb78ec25
Site Mao <site.mao1992@gmail.com>
2015-09-08 22:56:32 -0700

    Refactor and remove redundant code

--- RatingApp/app/src/main/java/com/example/matthew/ratingapp/RatingSaver.java
+++ RatingApp/app/src/main/java/com/example/matthew/ratingapp/RatingSaver.java
@@ -1,21 +1,19 @@
 package com.example.matthew.ratingapp;
+
 import android.content.Context;
 
-import java.io.*;
+import java.io.BufferedWriter;
 import java.io.FileOutputStream;
-
-/**
- * Created by Matthew on 8/18/2015.
- */
+import java.io.OutputStreamWriter;
 
 public class RatingSaver {
     public static void saveRating(float fun, float learn, Context app){
         try {
             FileOutputStream fos = app.openFileOutput(Data.FILE_NAME, Context.MODE_APPEND);
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-            bw.write(fun + "");
+            bw.write(Float.toString(fun));
             bw.newLine();
-            bw.write(learn + "");
+            bw.write(Float.toString(learn));
             bw.newLine();
             bw.close();
         }catch (Exception e){
